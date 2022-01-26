7d4b8b67ecc3267ea7220c055ba2f50b82d2d212
rehamalbulushi1998 <97521035+rehamalbulushi1998@users.noreply.github.com>
2022-01-26 13:03:07 +0400

    Create a relation between post and user models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'user_id',
+        'caregory_id',
+        'img',
+        'desciption',
+    ];
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
