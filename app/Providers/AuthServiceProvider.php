ece4011220ff95bb7f8c0bbf0dc2484ed0172b26
rehamalbulushi1998 <97521035+rehamalbulushi1998@users.noreply.github.com>
2022-01-27 00:11:41 +0400

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-
+use App\Models\Post;
+use App\Models\User;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -23,8 +24,11 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+       // $this->registerPolicies();
 
-        //
+       // Gate::define('create-post', function (User $user) {
+           // return $user->user_type === 'author';
+         //   dd($user);
+       // });
     }
 }
