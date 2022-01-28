9b781af4aeed0325ab8853f3c5c98e51a944c290
rehamalbulushi1998 <97521035+rehamalbulushi1998@users.noreply.github.com>
2022-01-28 13:10:38 +0400

    updating the blog page

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -14,7 +14,20 @@
 	<link rel="stylesheet" href="tempcss/slick.css">
 	<title>E-INK</title>
 
-
+<style>
+	 .frame {
+        width: 390px;
+        height: 340px;
+        border: 3px solid black;
+        background: #333;
+        margin: auto;
+        padding: 15px 10px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+	</style>
 </head>
 <body>
     <!-- Navigation -->
@@ -27,12 +40,14 @@
 	<section class="fh5co-blog">
 		<div class="site-container">
 			<h2 class="universal-h2 universal-h2-bckg">Blogs</h2>
-			<div class="blog-slider blog-inner">
+		
 				
 				@foreach ($post as $post )
 				<div class="single-blog">
 					<div class="single-blog__img">
-					<img src="/storage/{{ $post->img }}">
+					<div class="frame">
+                            <img src="/storage/{{ $post->img }}" />
+                        </div>
 					</div>
 					<div class="single-blog__text">
 						<h4>{{ $post->title }}</h4>
@@ -42,7 +57,7 @@
 				</div>
 				
 				@endforeach
-			</div>
+		
 		</div>
 	</section>
 
